9d2b900bd957781ea1ab1566a94a372d1a23b502
yokesh <yokeshthirumoorthi@gmail.com>
2022-03-19 18:43:20 -0700

    Add group_id column in Qamodel

--- backend/qaapp/models.py
+++ backend/qaapp/models.py
@@ -2,16 +2,21 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.utils import timezone
 
 User = get_user_model()
 
 
 class Qaapp(models.Model):
-    user = models.ForeignKey(User, models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.CASCADE)
+    group = models.ForeignKey(
+        Group, null=True, blank=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
     critical = models.BooleanField(default=False)
-    question = models.TextField()
-    answer = models.TextField()
+    question = models.TextField(default="")
+    answer = models.TextField(default="")
 
     def _str_(self):
         return self.question
